4f3aceee4699a4e5f126dfac95b3bcd1e1b3e65c
unknown <Bruno@KTI371.kryptus.lan>
2022-09-12 22:47:56 -0300

    animal dao

--- src/main/java/Model/AnimalDAO.java
+++ src/main/java/Model/AnimalDAO.java
@@ -4,40 +4,128 @@
  */
 package Model;
 
+import static Model.DAO.getConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Bruno
  */
-public class AnimalDAO {
-     private List<Animal> animal = new ArrayList<Animal> ();
- 
- 
-  public void insertAnimal (int id, String nome, char sexo, int idade, int idCliente, int idEspecie) {
-      
-     animal.add(new Animal(id,nome,sexo,idade,idCliente,idEspecie));
-  }
-  
-    public Animal readAnimal (int id){
-      
-     return animal.get(id);
-  }
+public class AnimalDAO extends DAO {
+      private static AnimalDAO instance;
+
+    private AnimalDAO() {
+        getConnection();
+        createTable();
+    }
+
+    // Singleton
+    public static AnimalDAO getInstance() {
+        return (instance==null?(instance = new AnimalDAO()):instance);
+    }
+
+// CRUD    
+    public Animal create(String nome, char sexo, int idade, int idEspecie, int idCliente) {
+        try {
+            PreparedStatement stmt;
+            stmt = DAO.getConnection().prepareStatement("INSERT INTO animal (nome, sexo, idade, idEspecie, idCliente) VALUES (?,?,?,?,?)");
+            stmt.setString(1, nome);
+            stmt.setString(2, sexo);
+            stmt.setInt(3, idade);
+            stmt.setInt(4, idEspecie);
+            stmt.setInt(5, idCliente);
+            executeUpdate(stmt);
+        } catch (SQLException ex) {
+            Logger.getLogger(AnimalDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return this.retrieveById(lastId("animal","id"));
+    }
     
-    public void deleteAnimal (int id){
+    // Uma pequena gambiarra, depois explico...
+    public boolean isLastEmpty(){
+        Animal lastAnimal = this.retrieveById(lastId("animal","id"));
+        if (lastAnimal != null) {
+            return lastAnimal.getNome().isBlank();
+        }
+        return false;
+    }
+
+    private Animal buildObject(ResultSet rs) {
+        Animal animal = null;
+        try {
+            animal = new Animal(rs.getInt("id"), rs.getString("nome"), rs.getString("sexo"), rs.getInt("idade"), rs.getInt("idEspecie"), rs.getInt("idCliente"));
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+        return animal;
+    }
+
+    // Generic Retriever
+    public List retrieve(String query) {
+        List<Animal> animais = new ArrayList();
+        ResultSet rs = getResultSet(query);
+        try {
+            while (rs.next()) {
+                animais.add(buildObject(rs));
+            }
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+        return animais;
+    }
+    
+    // RetrieveAll
+    public List retrieveAll() {
+        return this.retrieve("SELECT * FROM animal");
+    }
+    
+    // RetrieveLast
+    public List retrieveLast(){
+        return this.retrieve("SELECT * FROM animal WHERE id = " + lastId("animal","id"));
+    }
+
+    // RetrieveById
+    public Animal retrieveById(int id) {
+        List<Cliente> animais = this.retrieve("SELECT * FROM animal WHERE id = " + id);
+        return (animais.isEmpty()?null:animais.get(0));
+    }
+
+    // RetrieveBySimilarName
+    public List retrieveBySimilarName(String nome) {
+        return this.retrieve("SELECT * FROM animal WHERE nome LIKE '%" + nome + "%'");
+    }    
         
-        animal.remove(id);
+    // Updade
+    public void update(Animal animal) {
+        try {
+            PreparedStatement stmt;
+            stmt = DAO.getConnection().prepareStatement("UPDATE animal SET nome=?, sexo=?, idade=?, idEspecie=?, idCliente=? WHERE id=?");
+            stmt.setString(1, animal.getNome());
+            stmt.setString(2, animal.getSexo());
+            stmt.setString(3, animal.getIdade());
+            stmt.setString(4, animal.getIdEspecie());
+            stmt.setString(5, animal.getIdCliente());
+            stmt.setInt(6, animal.getId());
+            executeUpdate(stmt);
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
     }
-    
-    public void updateAnimal(int id, String nome, char sexo, int idade, int idCliente, int idEspecie){
-        
-        animal.remove(id);
-        animal.add(new Animal(id,nome,sexo,idade,idCliente, idEspecie));
+        // Delete   
+    public void delete(Animal animal) {
+        PreparedStatement stmt;
+        try {
+            stmt = DAO.getConnection().prepareStatement("DELETE FROM animal WHERE id = ?");
+            stmt.setInt(1, animal.getId());
+            executeUpdate(stmt);
+        } catch (SQLException e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
     }
-    
-    public int getSize(){
-        return animal.size();
-    }
-    
 }
